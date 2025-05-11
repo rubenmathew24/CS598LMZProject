ed1f46fdb1513bf85ce58731534eaf7109855cba
Alex Eilert <alexjeilert@gmail.com>
2025-05-11 03:50:18 -0500

    testing

--- Agentless/agentless/test/run_tests.py
+++ Agentless/agentless/test/run_tests.py
@@ -18,12 +18,12 @@ from swebench.harness.constants import (
 )
 from swebench.harness.docker_build import build_env_images
 from swebench.harness.run_evaluation import get_dataset_from_preds, run_instance
-from swebench.harness.test_spec import (
+from swebench.harness.test_spec.test_spec import (
     TestSpec,
     make_env_script_list,
     make_repo_script_list,
 )
-from swebench.harness.utils import get_test_directives
+from swebench.harness.test_spec.python import get_test_directives
 from tqdm import tqdm
 
 OPEN_FILE_LIMIT = 4096
@@ -506,6 +506,8 @@ def run_tests(
     print(f"Using run_id: {run_id}")
 
     split = "test"
+    print("HERE")
+    print(os.getenv("DOCKER_HOST"))
     client = docker.from_env()
     force_rebuild = False
 
